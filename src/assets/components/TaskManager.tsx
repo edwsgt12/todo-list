838f8deb4736680560e0282c79496a4058e3cef6
edwsgt67 <edward.curso.programadorpro@gmail.com>
2026-02-08 12:27:46 -0400

    api_2

--- src/assets/components/TaskManager.tsx
+++ src/assets/components/TaskManager.tsx
@@ -1,7 +1,7 @@
 import { use, useEffect, useState } from "react";
 import { VscDebugBreakpointUnsupported } from "react-icons/vsc";
 import TaskInput from "./TaskImput";
-import TaskList from "./TaskList";
+import TaskList from "./TaskList";  
 
 export interface ITask {
     id: number;
@@ -13,7 +13,7 @@ const API_URL = import.meta.env.VITE_TODO_LIST_API;
 const GET_LIST_URL = `${API_URL}/getToDoList`;
 const INSERT_LIST_URL = `${API_URL}/insertToDoList`;
 const UPDATE_LIST_URL = `${API_URL}/updateToDoList`;
-
+   
 const TaskManager = () => {
     const [task, setTask] = useState<ITask[]>([]);
     const [loading, setLoading] = useState(false);
